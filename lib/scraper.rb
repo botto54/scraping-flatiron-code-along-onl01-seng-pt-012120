bd30cd611e9dced68c13fb6e0b5080626b0de5a7
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-30 22:38:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,14 +6,18 @@ require_relative './course.rb'
 class Scraper
   
   # The #get_page instance method will be responsible for using Nokogiri and open-uri to grab the entire HTML document from the web page.
+ 
   def get_page
     
   end
   
   # The #get_courses instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course. In other words, the return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering. We're going to have to examine the page with the element inspector to find the CSS selector that contains the courses.
+ 
   def get_courses
     
   end
+  
+  
   
   def make_courses
     
