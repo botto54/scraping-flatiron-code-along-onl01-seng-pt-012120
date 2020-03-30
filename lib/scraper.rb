6c0ab421b9b6ce8328167b895c5847e950c30ce8
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-30 22:55:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,13 +21,18 @@ class Scraper
   # The #get_courses instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course. In other words, the return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering. We're going to have to examine the page with the element inspector to find the CSS selector that contains the courses.
  
   def get_courses
-    
+    self.get_page.css(".post")
   end
   
   # The #make_courses method will be responsible for actually instantiating Course objects and giving each course object the correct title, schedule and description attribute that we scraped from the page.
   
   def make_courses
-    
+    self.get_courses.each do |post|
+            course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
   
   
