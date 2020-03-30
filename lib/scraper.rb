2bdf34402b3207ac30bdbe212d27c8f998e132ac
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-30 22:51:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,7 @@ class Scraper
     #   course.title = post.css("h2").text
     #   course.schedule = post.css(".date").text
     #   course.description = post.css("p").text
-    end
+    #end
   end
   
   # The #get_courses instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course. In other words, the return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering. We're going to have to examine the page with the element inspector to find the CSS selector that contains the courses.
